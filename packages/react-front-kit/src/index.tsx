b4bf3ac324d6a46b414e104a57357f80d223af41
Quentin Le Caignec <quentin.lecaignec@smile.fr>
2023-09-04 17:41:46 +0200

    fix: tmp

--- packages/react-front-kit/src/index.tsx
+++ packages/react-front-kit/src/index.tsx
@@ -4,6 +4,7 @@ export * from './3-custom/Components/SidebarMenu/SidebarMenu';
 export * from './3-custom/Components/HeaderSearch/HeaderSearch';
 export * from './3-custom/Components/DropdownButton/DropdownButton';
 export * from './3-custom/Components/Header/Header';
+export * from './3-custom/Components/Pagination/Pagination';
 export * from './3-custom/Pages/TestPage/TestPage';
 export * from './3-custom/Provider/Provider';
 export * from './theme';
