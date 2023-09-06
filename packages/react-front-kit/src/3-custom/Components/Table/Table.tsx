f03f7b4287aa7d4a9aeebbe02b6692faff53cb78
vapersmile <valentin.perello@smile.fr>
2023-09-06 17:47:08 +0200

    feat(readmine 244989): add modals + add style for action menu

--- packages/react-front-kit/src/3-custom/Components/Table/Table.tsx
+++ packages/react-front-kit/src/3-custom/Components/Table/Table.tsx
@@ -1,15 +1,19 @@
 'use client';
 
 import type { MRT_ColumnDef } from 'mantine-react-table';
+import type { ReactNode } from 'react';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
+import { ActionIcon, Button, Menu, Modal, Tooltip } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { ShareNetwork, Star, Trash } from '@phosphor-icons/react';
 import { MantineReactTable, useMantineReactTable } from 'mantine-react-table';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
 interface IDocument {
   creator: string;
   date: string;
   format: string;
+  id: number | string;
   title: string;
 }
 
@@ -47,12 +51,12 @@ const edit = (
     width="12"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_819_2424)">
+    <g clipPath="url(#clip0_819_2424)">
       <path
         d="M8.5 1.50003C8.63132 1.36871 8.78722 1.26454 8.9588 1.19347C9.13038 1.1224 9.31428 1.08582 9.5 1.08582C9.68572 1.08582 9.86962 1.1224 10.0412 1.19347C10.2128 1.26454 10.3687 1.36871 10.5 1.50003C10.6313 1.63135 10.7355 1.78725 10.8066 1.95883C10.8776 2.13041 10.9142 2.31431 10.9142 2.50003C10.9142 2.68575 10.8776 2.86964 10.8066 3.04123C10.7355 3.21281 10.6313 3.36871 10.5 3.50003L3.75 10.25L1 11L1.75 8.25003L8.5 1.50003Z"
         stroke="#5C5F66"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </g>
     <defs>
@@ -73,71 +77,79 @@ const arbo = (
     <path
       d="M3.58333 10H1.41667C1.30616 10 1.20018 9.95564 1.12204 9.87668C1.0439 9.79771 1 9.69062 1 9.57895V2.42105C1 2.30938 1.0439 2.20229 1.12204 2.12332C1.20018 2.04436 1.30616 2 1.41667 2H4.19271C4.2828 2.00037 4.37041 2.02988 4.44271 2.08421L5.89062 3.17895C5.96292 3.23327 6.05053 3.26278 6.14062 3.26316H10.5833C10.6938 3.26316 10.7998 3.30752 10.878 3.38648C10.9561 3.46544 11 3.57254 11 3.68421V5.36842"
       stroke="#5C5F66"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M6 9H11"
       stroke="#5C5F66"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M9 7L11 9L9 11"
       stroke="#5C5F66"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
-
-// styles
-
-const actionButton = {
-  background: 'transparent',
-  border: 'none',
-  cursor: 'pointer',
-};
 
 const data: IDocument[] = [
   {
     creator: 'Valentin Perello',
     date: '20/05/2022',
     format: 'SVG',
+    id: 1,
     title: 'Doc test',
   },
   {
     creator: 'Valentin Perello',
     date: '20/05/2022',
     format: 'PDF',
+    id: 2,
     title: 'Doc test',
   },
   {
     creator: 'Valentin Perello',
     date: '20/05/2022',
     format: 'PDF',
+    id: 3,
     title: 'Doc test',
   },
   {
     creator: 'Valentin Perello',
     date: '20/05/2022',
     format: 'PDF',
+    id: 4,
     title: 'Doc test',
   },
   {
     creator: 'Valentin Perello',
     date: '20/05/2022',
     format: 'PDF',
+    id: 5,
     title: 'Doc test',
   },
 ];
 
 export function Table(): JSX.Element {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const [modalTitle, setModalTitle] = useState<string>('');
+  const [modalContent, setModalContent] = useState<ReactNode | undefined>();
+  const [currentElement, setCurrentElement] = useState<
+    number | string | IDocument
+  >(0);
+
   const columns = useMemo<MRT_ColumnDef<IDocument>[]>(
     () => [
       {
+        accessorKey: 'id',
+        header: 'id',
+      },
+      {
         accessorKey: 'format',
-        columnFilterModeOptions: ['fuzzy', 'contains', 'startsWith'],
         header: 'Format',
       },
       {
@@ -160,8 +172,8 @@ export function Table(): JSX.Element {
     data,
     displayColumnDefOptions: {
       'mrt-row-actions': {
-        header: undefined, // change header text
-        size: 124, // make actions column wider
+        header: undefined,
+        size: 124,
       },
     },
     enableColumnOrdering: false,
@@ -171,9 +183,14 @@ export function Table(): JSX.Element {
     enableRowSelection: true,
     initialState: {
       columnPinning: {
-        right: ['mrt-row-actions'], // pin built-in row actions display column to right by default
+        right: ['mrt-row-actions'],
       },
     },
+    mantineTableBodyRowProps: ({ row }) => ({
+      onClick: () => {
+        setCurrentElement(row.original);
+      },
+    }),
     positionActionsColumn: 'last',
     renderRowActions: () => (
       <div
@@ -186,18 +203,132 @@ export function Table(): JSX.Element {
           marginRight: '16px',
         }}
       >
-        <button style={actionButton} type="button">
-          {arbo}
-        </button>
-        <button style={actionButton} type="button">
-          {edit}
-        </button>
-        <button style={actionButton} type="button">
-          {menu}
-        </button>
+        {tooltip(
+          <ActionIcon onClick={arboHandle} radius={4} type="button">
+            {arbo}
+          </ActionIcon>,
+          'Déplacer dans l’arborescence'
+        )}
+        {tooltip(
+          <ActionIcon onClick={editHandle} radius={4} type="button">
+            {edit}
+          </ActionIcon>,
+          'Modifier'
+        )}
+        {tooltip(
+          <ActionIcon radius={4} type="button">
+            {menuAction}
+          </ActionIcon>,
+          "Plus d'options"
+        )}
       </div>
     ),
   });
 
-  return <MantineReactTable table={table} />;
+  // Component
+  const tooltip = (element: ReactNode, text: string): ReactNode => {
+    return (
+      <Tooltip
+        color="#495057"
+        label={text}
+        position="bottom"
+        radius={6}
+        withArrow
+        withinPortal
+      >
+        {element}
+      </Tooltip>
+    );
+  };
+
+  // Handle
+  const addToFavHandle = (): void => {
+    setModalTitle(
+      'Êtes vous sure de vouloir ajouter aux favoris cette élément ?'
+    );
+    setModalContent(
+      <>
+        <Button
+          onClick={sendCurrentElementValue}
+          style={{ marginRight: '10px' }}
+        >
+          Oui
+        </Button>
+        <Button onClick={close}>Non</Button>
+      </>
+    );
+    open();
+  };
+  const shareHandle = (): void => {
+    setModalTitle('SHARE');
+    open();
+  };
+  const editHandle = (): void => {
+    setModalTitle('EDIT');
+    open();
+  };
+  const arboHandle = (): void => {
+    setModalTitle("Changer l'arborecence ?");
+    open();
+  };
+  const removeHandle = (): void => {
+    setModalTitle('Êtes vous sure de vouloir supprimer cette élément ?');
+    setModalContent(
+      <>
+        <Button
+          onClick={sendCurrentElementValue}
+          style={{ marginRight: '10px' }}
+        >
+          Oui
+        </Button>
+        <Button onClick={close}>Non</Button>
+      </>
+    );
+    open();
+  };
+
+  const sendCurrentElementValue = (): void => {
+    console.log(currentElement);
+    close();
+  };
+
+  const menuAction = (
+    <Menu radius={4} shadow="lg" width={200} withinPortal>
+      <Menu.Target>
+        <div>{menu}</div>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Item icon={<Star size={14} />} onClick={addToFavHandle}>
+          Ajout aux favoris
+        </Menu.Item>
+        <Menu.Item icon={<ShareNetwork size={14} />} onClick={shareHandle}>
+          Partager
+        </Menu.Item>
+        <Menu.Item
+          color="red"
+          icon={<Trash size={14} />}
+          onClick={removeHandle}
+        >
+          Supprimer
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  );
+
+  return (
+    <>
+      <MantineReactTable table={table} />
+      <Modal
+        centered
+        className="removeElementModal"
+        onClose={close}
+        opened={opened}
+        radius={4}
+        title={modalTitle}
+      >
+        {modalContent}
+      </Modal>
+    </>
+  );
 }
