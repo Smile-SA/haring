454daae1be3ce14e43cd91c5b00c362826ef9429
vapersmile <valentin.perello@smile.fr>
2023-09-15 15:53:34 +0200

    feat: eslint-disable for any error

--- packages/react-front-kit/src/3-custom/Components/Table/Table.test.tsx
+++ packages/react-front-kit/src/3-custom/Components/Table/Table.test.tsx
@@ -3,7 +3,9 @@ import { renderWithProviders } from '../../../utils/tests';
 import { Table } from './Table';
 
 jest.mock('@mantine/hooks', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const original = jest.requireActual('@mantine/hooks');
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return {
     ...original,
     useId: jest.fn(),
