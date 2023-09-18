80c51d5722838343481a6c0a99c4b44751794121
Tony Cabaye <tony.cabaye@smile.fr>
2023-09-18 16:10:33 +0200

    feat: add custom icons (#6)

--- packages/react-front-kit/src/icons.tsx
+++ packages/react-front-kit/src/icons.tsx
@@ -1,14 +1,19 @@
+/* eslint-disable react-refresh/only-export-components */
 import type { Icon } from '@phosphor-icons/react';
 
-import * as Icons from '@phosphor-icons/react';
+import * as ppIcons from '@phosphor-icons/react';
+
+import * as customIcons from './1-styleGuide/Icons';
 
 const iconsToIgnore = ['IconBase', 'IconContext'];
-const iconEntries = Object.entries(Icons).filter(
+const phosphorIconsEntries = Object.entries(ppIcons).filter(
   ([key]) => !iconsToIgnore.includes(key),
 ) as [string, Icon][];
 
-export const icons = Object.fromEntries(iconEntries);
+export * as customIcons from './1-styleGuide/Icons';
+export const phosphorIcons = Object.fromEntries(phosphorIconsEntries);
+export const icons: Record<string, Icon> = { ...customIcons, ...phosphorIcons };
 
 export const iconsElements = Object.fromEntries(
-  iconEntries.map(([key, Icon]) => [key, <Icon key={key} />]),
+  Object.entries(icons).map(([key, Icon]) => [key, <Icon key={key} />]),
 );
