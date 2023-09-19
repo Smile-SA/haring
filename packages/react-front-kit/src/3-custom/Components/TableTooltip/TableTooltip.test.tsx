6beb53241a0c164043b9205c8e731d719f9bb275
vapersmile <valentin.perello@smile.fr>
2023-09-19 09:47:34 +0200

    feat(redmine 1245001): fix eslint errors

--- packages/react-front-kit/src/3-custom/Components/TableTooltip/TableTooltip.test.tsx
+++ packages/react-front-kit/src/3-custom/Components/TableTooltip/TableTooltip.test.tsx
@@ -1,10 +1,14 @@
+import { Button } from '@mantine/core';
+
 import { renderWithProviders } from '../../../utils/tests';
 
 import { TableTooltip } from './TableTooltip';
 
 describe('TableTooltip', () => {
   it('matches snapshot', () => {
-    const { container } = renderWithProviders(<TableTooltip />);
+    const { container } = renderWithProviders(
+      <TableTooltip element={<Button>test</Button>} text="test" />,
+    );
     expect(container).toMatchSnapshot();
   });
 });
