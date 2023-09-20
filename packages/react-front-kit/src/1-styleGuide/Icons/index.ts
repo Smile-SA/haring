fecc710c3009164bbb7479afc579f02d2178daa2
vapersmile <valentin.perello@smile.fr>
2023-09-20 12:13:27 +0200

    feat(redmine 1244979): change import method for icon and add comment eslint ignore

--- packages/react-front-kit/src/1-styleGuide/Icons/index.ts
+++ packages/react-front-kit/src/1-styleGuide/Icons/index.ts
@@ -1,3 +1,6 @@
 /* eslint react-refresh/only-export-components: 0 */
+export * from './Edit';
 export * from './ColumnPlus';
 export * from './FolderMove';
+export * from './MenuTable';
+export * from './TreeStructure';
--- packages/react-front-kit/src/3-custom/Components/Table/Table.tsx
+++ packages/react-front-kit/src/3-custom/Components/Table/Table.tsx
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment*/
+/* eslint-disable @typescript-eslint/no-unsafe-member-access*/
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
 'use client';
 import type { MRT_ColumnDef } from 'mantine-react-table';
 import type { ReactNode } from 'react';
@@ -32,6 +38,10 @@ import {
 import { MRT_Localization_FR } from 'mantine-react-table/locales/fr';
 import { useState } from 'react';
 
+import { ColumnPlus } from '../../../1-styleGuide/Icons/ColumnPlus';
+import { Edit } from '../../../1-styleGuide/Icons/Edit';
+import { MenuTable } from '../../../1-styleGuide/Icons/MenuTable';
+import { TreeStructure } from '../../../1-styleGuide/Icons/TreeStructure';
 import { TableTooltip } from '../TableTooltip/TableTooltip';
 
 interface IDocument {
@@ -47,84 +57,6 @@ interface IProps {
   columns: MRT_ColumnDef<IDocument>[];
   data: IDocument[];
 }
-
-const menu = (
-  <svg
-    fill="none"
-    height="12"
-    style={{ margin: 'auto' }}
-    viewBox="0 0 12 12"
-    width="12"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M6 3.375C6.20711 3.375 6.375 3.20711 6.375 3C6.375 2.79289 6.20711 2.625 6 2.625C5.79289 2.625 5.625 2.79289 5.625 3C5.625 3.20711 5.79289 3.375 6 3.375Z"
-      fill="#5C5F66"
-      stroke="#5C5F66"
-    />
-    <path
-      d="M6 6.375C6.20711 6.375 6.375 6.20711 6.375 6C6.375 5.79289 6.20711 5.625 6 5.625C5.79289 5.625 5.625 5.79289 5.625 6C5.625 6.20711 5.79289 6.375 6 6.375Z"
-      fill="#5C5F66"
-      stroke="#5C5F66"
-    />
-    <path
-      d="M6 9.375C6.20711 9.375 6.375 9.20711 6.375 9C6.375 8.79289 6.20711 8.625 6 8.625C5.79289 8.625 5.625 8.79289 5.625 9C5.625 9.20711 5.79289 9.375 6 9.375Z"
-      fill="#5C5F66"
-      stroke="#5C5F66"
-    />
-  </svg>
-);
-const edit = (
-  <svg
-    fill="none"
-    height="12"
-    viewBox="0 0 12 12"
-    width="12"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g clipPath="url(#clip0_819_2424)">
-      <path
-        d="M8.5 1.50003C8.63132 1.36871 8.78722 1.26454 8.9588 1.19347C9.13038 1.1224 9.31428 1.08582 9.5 1.08582C9.68572 1.08582 9.86962 1.1224 10.0412 1.19347C10.2128 1.26454 10.3687 1.36871 10.5 1.50003C10.6313 1.63135 10.7355 1.78725 10.8066 1.95883C10.8776 2.13041 10.9142 2.31431 10.9142 2.50003C10.9142 2.68575 10.8776 2.86964 10.8066 3.04123C10.7355 3.21281 10.6313 3.36871 10.5 3.50003L3.75 10.25L1 11L1.75 8.25003L8.5 1.50003Z"
-        stroke="#5C5F66"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </g>
-    <defs>
-      <clipPath id="clip0_819_2424">
-        <rect fill="white" height="12" width="12" />
-      </clipPath>
-    </defs>
-  </svg>
-);
-const arbo = (
-  <svg
-    fill="none"
-    height="12"
-    viewBox="0 0 12 12"
-    width="12"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M3.58333 10H1.41667C1.30616 10 1.20018 9.95564 1.12204 9.87668C1.0439 9.79771 1 9.69062 1 9.57895V2.42105C1 2.30938 1.0439 2.20229 1.12204 2.12332C1.20018 2.04436 1.30616 2 1.41667 2H4.19271C4.2828 2.00037 4.37041 2.02988 4.44271 2.08421L5.89062 3.17895C5.96292 3.23327 6.05053 3.26278 6.14062 3.26316H10.5833C10.6938 3.26316 10.7998 3.30752 10.878 3.38648C10.9561 3.46544 11 3.57254 11 3.68421V5.36842"
-      stroke="#5C5F66"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M6 9H11"
-      stroke="#5C5F66"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-    <path
-      d="M9 7L11 9L9 11"
-      stroke="#5C5F66"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    />
-  </svg>
-);
 
 const useStyles = createStyles((theme) => ({
   buttonFilters: { background: 'white', height: '34px', marginRight: '10px' },
@@ -286,7 +218,7 @@ export function Table(props: IProps): JSX.Element {
     open();
   }
   function handleTree(currentElement: IDocument): void {
-    action('ARBO_CHANGE_LOCATION', currentElement);
+    action('TREE_STRUCTURE_CHANGE_LOCATION', currentElement);
   }
   function handleRemove(currentElement: IDocument): void {
     setModalContent(
@@ -381,6 +313,7 @@ export function Table(props: IProps): JSX.Element {
     enableRowSelection: true,
     icons: {
       IconArrowsSort: CaretUpDown,
+      IconColumns: ColumnPlus,
       // eslint-disable-next-line react/no-unstable-nested-components, react/no-multi-comp
       IconFilter: () => <Funnel size={18} />,
       // eslint-disable-next-line react/no-unstable-nested-components, react/no-multi-comp
@@ -430,7 +363,7 @@ export function Table(props: IProps): JSX.Element {
               radius={4}
               type="button"
             >
-              {arbo}
+              <TreeStructure />
             </ActionIcon>
           }
           text="Déplacer dans l’arborescence"
@@ -464,7 +397,7 @@ export function Table(props: IProps): JSX.Element {
               radius={4}
               type="button"
             >
-              {edit}
+              <Edit />
             </ActionIcon>
           }
           text="Modifier le document"
@@ -478,7 +411,7 @@ export function Table(props: IProps): JSX.Element {
                     className={classes.menuButton}
                     style={{ display: 'flex', height: '28px', width: '28px' }}
                   >
-                    {menu}
+                    <MenuTable />
                   </div>
                 </Menu.Target>
 
@@ -531,12 +464,12 @@ export function Table(props: IProps): JSX.Element {
             onClick={() => {
               sendSelectedElementsValueWithAction(
                 cell.table.getSelectedRowModel().rows.map((x) => x.original),
-                'ARBO_CHANGE_LOCATION',
+                'MULTI_TREE_STRUCTURE_CHANGE_LOCATION',
               );
             }}
             variant="default"
           >
-            Déplacer dans l&apos;arborecence
+            Déplacer dans l&apos;arborescence
           </Button>
           <Button
             className={classes.buttonsToolbarAlertRemove}
@@ -561,6 +494,7 @@ export function Table(props: IProps): JSX.Element {
         />
         <MRTShowHideColumnsButton
           className={classes.buttonsShowHideColumns}
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
           table={cell.table}
         />
       </Box>
