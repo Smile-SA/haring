128a340ccca9d2872dc21b7ea79199201b84b049
Tony Cabaye <tony.cabaye@smile.fr>
2023-09-26 11:17:54 +0200

    fix: custom icon colors + table tooltips (#19)

--- packages/react-front-kit/src/1-styleGuide/Icons/ColumnPlus.tsx
+++ packages/react-front-kit/src/1-styleGuide/Icons/ColumnPlus.tsx
@@ -10,33 +10,24 @@ const weights = new Map<IconWeight, ReactElement>([
   [
     'regular',
     <>
-      <path
-        d="M3 1.86005V10.11"
-        stroke="black"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
+      <path d="M3 1.86005V10.11" strokeLinecap="round" strokeLinejoin="round" />
       <path
         d="M5.99988 1.875V10.125"
-        stroke="black"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M9.0083 1.88995V6.76495"
-        stroke="black"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M8.9812 8.63989V10.8899"
-        stroke="black"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M7.8562 9.76489H10.1062"
-        stroke="black"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
@@ -46,11 +37,13 @@ const weights = new Map<IconWeight, ReactElement>([
 
 export const ColumnPlus: Icon = forwardRef<SVGSVGElement, IconProps>(
   function ColumnPlus(props, ref) {
+    const { color = 'currentColor', ...iconProps } = props;
     return (
       <IconBase
         ref={ref}
-        {...props}
+        {...iconProps}
         fill="none"
+        stroke={color}
         viewBox="0 0 12 12"
         weights={weights}
       />
--- packages/react-front-kit/src/1-styleGuide/Icons/index.ts
+++ packages/react-front-kit/src/1-styleGuide/Icons/index.ts
@@ -1,6 +1,3 @@
 /* eslint react-refresh/only-export-components: 0 */
-export * from './Edit';
 export * from './ColumnPlus';
 export * from './FolderMove';
-export * from './MenuTable';
-export * from './TreeStructure';
