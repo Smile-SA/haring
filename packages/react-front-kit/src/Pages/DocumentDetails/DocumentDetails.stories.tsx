a77fc1a5bd6ed87425a8db9db621b441f51e49c3
Quentin Le Caignec <quentin.lecaignec@smile.fr>
2023-10-19 16:09:11 +0200

    fix: code review

--- packages/react-front-kit/src/Pages/DocumentDetails/DocumentDetails.stories.tsx
+++ packages/react-front-kit/src/Pages/DocumentDetails/DocumentDetails.stories.tsx
@@ -2,7 +2,7 @@
 
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { DocumentDetails as Cmp } from './DocumentDetails';
+import Cmp from './DocumentDetails';
 
 const meta = {
   component: Cmp,
