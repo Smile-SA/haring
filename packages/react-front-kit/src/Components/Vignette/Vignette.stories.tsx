c996c1133815497fafeaf7f54ff4a0602eaca290
vapersmile <valentin.perello@smile.fr>
2023-10-30 17:20:50 +0100

    feat(redmine 1246945): add icons map, menu, optimisation and set background color

--- packages/react-front-kit/src/Components/Vignette/Vignette.stories.tsx
+++ packages/react-front-kit/src/Components/Vignette/Vignette.stories.tsx
@@ -1,6 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Plus } from '@phosphor-icons/react';
+import {
+  DownloadSimple,
+  Eye,
+  PencilSimple,
+  ShareNetwork,
+  Star,
+  Trash,
+} from '@phosphor-icons/react';
+import { FolderMove } from '@smile/react-front-kit-shared';
 import { action } from '@storybook/addon-actions';
 
 import { Vignette as Cmp } from './Vignette';
@@ -18,13 +26,45 @@ export const Vignette: IStory = {
   args: {
     action: [
       {
-        icon: <Plus size="16px" />,
-        label: 'Add',
-        onAction: action('onAction'),
+        icon: <FolderMove />,
+        label: 'Move in tree',
+        onAction: action('Move in tree'),
+      },
+      {
+        icon: <Eye />,
+        label: 'Open document',
+        onAction: action('Open document'),
+      },
+      {
+        icon: <PencilSimple />,
+        label: 'Edit document',
+        onAction: action('Edit document'),
+      },
+      {
+        icon: <Star />,
+        label: 'Add to favorites',
+        onAction: action('Add to favorites'),
+      },
+      {
+        icon: <ShareNetwork />,
+        label: 'Share',
+        onAction: action('Share'),
+      },
+      {
+        icon: <DownloadSimple />,
+        label: 'Download',
+        onAction: action('Download'),
+      },
+      {
+        color: 'red',
+        icon: <Trash />,
+        label: 'Delete',
+        onAction: action('Delete'),
       },
     ],
-    icon: <Plus size="16px" />,
-    label: 'Document',
+    iconType: 'PDF',
+    label: 'Document long and boring for a long exemple of ellipse',
+    onClicked: action('onClick'),
     selected: false,
   },
 };
--- packages/react-front-kit/src/Components/Vignette/Vignette.tsx
+++ packages/react-front-kit/src/Components/Vignette/Vignette.tsx
@@ -1,18 +1,67 @@
 'use client';
 
+import type { MantineColor } from '@mantine/core';
 import type { ReactElement, ReactNode } from 'react';
 
 import {
+  ActionIcon,
   Box,
   Group,
   Image,
+  Menu,
+  Text,
   createStyles,
   useMantineTheme,
 } from '@mantine/core';
+import {
+  DotsThreeVertical,
+  File,
+  FileCss,
+  FileCsv,
+  FileDoc,
+  FileHtml,
+  FileJpg,
+  FileJs,
+  FileJsx,
+  FilePdf,
+  FilePng,
+  FilePpt,
+  FileRs,
+  FileSql,
+  FileSvg,
+  FileTs,
+  FileTsx,
+  FileVue,
+  FileXls,
+  FileZip,
+} from '@phosphor-icons/react';
 
 import defaultImage from './defaultImage.jpg';
 
 const useStyles = createStyles((theme) => ({
+  headerContainer: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    justifyContent: 'space-between',
+    marginBottom: '22px',
+    marginTop: '10px',
+  },
+  icon: {
+    height: 'fit-content',
+    margin: 'auto',
+  },
+  menuButton: {
+    '&[aria-expanded=true]': {
+      '& svg': {
+        filter: 'contrast(8) invert(1)',
+      },
+      backgroundColor: theme.colors.cyan[9],
+      borderRadius: '4px',
+      display: 'flex',
+      height: '28px',
+      width: '28px',
+    },
+  },
   root: {
     borderRadius: '16px',
     heigh: 'auto',
@@ -21,28 +70,96 @@ const useStyles = createStyles((theme) => ({
   },
   rootSelected: { background: theme.primaryColor },
   title: {
-    margin: '0',
+    margin: '0 19px 0 0',
     paddingLeft: '10px',
   },
   titleContainer: {
     alignItems: 'center',
     display: 'flex',
     justifyContent: 'center',
-    marginBottom: '22px',
-    marginTop: '10px',
-  },
-  icon: {
-    height: 'fit-content',
-    margin: 'auto',
+    overflow: 'hidden',
   },
 }));
 
+export type IFileExtendType =
+  | 'CSS'
+  | 'CSV'
+  | 'DOC'
+  | 'HTML'
+  | 'JPG'
+  | 'JS'
+  | 'JSX'
+  | 'PDF'
+  | 'PNG'
+  | 'PPT'
+  | 'RS'
+  | 'SQL'
+  | 'SVG'
+  | 'TS'
+  | 'TSX'
+  | 'UNKNOWN'
+  | 'VUE'
+  | 'XLS'
+  | 'ZIP';
+
+const getIconByIconType = (
+  type: IFileExtendType,
+  color: MantineColor,
+): ReactElement => {
+  switch (type) {
+    case 'CSS':
+      return <FileCss color={color} size={20} weight="bold" />;
+    case 'DOC':
+      return <FileDoc color={color} size={20} weight="bold" />;
+    case 'PDF':
+      return <FilePdf color={color} size={20} weight="bold" />;
+    case 'CSV':
+      return <FileCsv color={color} size={20} weight="bold" />;
+    case 'HTML':
+      return <FileHtml color={color} size={20} weight="bold" />;
+    case 'JPG':
+      return <FileJpg color={color} size={20} weight="bold" />;
+    case 'JS':
+      return <FileJs color={color} size={20} weight="bold" />;
+    case 'JSX':
+      return <FileJsx color={color} size={20} weight="bold" />;
+    case 'PNG':
+      return <FilePng color={color} size={20} weight="bold" />;
+    case 'PPT':
+      return <FilePpt color={color} size={20} weight="bold" />;
+    case 'RS':
+      return <FileRs color={color} size={20} weight="bold" />;
+    case 'SQL':
+      return <FileSql color={color} size={20} weight="bold" />;
+    case 'SVG':
+      return <FileSvg color={color} size={20} weight="bold" />;
+    case 'TS':
+      return <FileTs color={color} size={20} weight="bold" />;
+    case 'TSX':
+      return <FileTsx color={color} size={20} weight="bold" />;
+    case 'VUE':
+      return <FileVue color={color} size={20} weight="bold" />;
+    case 'XLS':
+      return <FileXls color={color} size={20} weight="bold" />;
+    case 'ZIP':
+      return <FileZip color={color} size={20} weight="bold" />;
+    default: {
+      return <File color={color} size={20} weight="bold" />;
+    }
+  }
+};
+
 export interface IVignetteProps {
-  action?: [{ icon: ReactNode; label: string; onAction: () => void }];
-  icon?: ReactElement;
+  action?: {
+    color?: string;
+    icon: ReactNode;
+    label: string;
+    onAction: () => void;
+  }[];
+  iconType?: IFileExtendType;
   image?: ReactElement;
   label?: string;
-  onClick?: () => void;
+  onClicked?: () => void;
   selected: boolean;
 }
 
@@ -50,18 +167,15 @@ export function Vignette(props: IVignetteProps): ReactElement {
   const { classes } = useStyles();
   const theme = useMantineTheme();
   const {
-    action,
-    icon,
-    image = <Image radius="14px" src={defaultImage} />,
+    action = [],
+    iconType = 'UNKNOWN',
+    image = defaultImage,
     label,
-    onClick = (selected: boolean) => {
-      selected = !selected;
-    },
+    onClicked,
     selected = false,
   } = props;
   function getGoodTextColor(): string {
-    console.log(theme.primaryShade);
-    if (theme.primaryShade > 5) {
+    if (typeof theme.primaryShade === `number` && theme.primaryShade > 5) {
       return 'white';
     }
     return 'black';
@@ -69,23 +183,63 @@ export function Vignette(props: IVignetteProps): ReactElement {
 
   return (
     <Box
-      bg={String(Boolean(selected) && theme.primaryColor)}
+      bg={String(selected ? theme.primaryColor : theme.colors.gray[1])}
       className={classes.root}
-      onClick={() => onClick(selected)}
-      radius="22px"
+      onClick={onClicked}
     >
-      <Group>
+      <Group className={classes.headerContainer}>
         <div className={classes.titleContainer}>
-          {icon}
-          <h3
+          {getIconByIconType(
+            iconType,
+            String(selected ? theme.colors.gray[1] : theme.colors.cyan[9]),
+          )}
+          <Text
             className={classes.title}
-            style={{ color: selected && getGoodTextColor() }}
+            component="h3"
+            style={{
+              color: selected ? getGoodTextColor() : undefined,
+            }}
+            truncate
           >
             {label}
-          </h3>
+          </Text>
+        </div>
+        <div>
+          {action.length > 0 && (
+            <Menu radius={4} shadow="lg" width={200} withinPortal>
+              <Menu.Target>
+                <ActionIcon
+                  className={classes.menuButton}
+                  radius={4}
+                  type="button"
+                >
+                  <div>
+                    <DotsThreeVertical
+                      color={
+                        selected ? theme.colors.gray[1] : theme.colors.cyan[9]
+                      }
+                      size={16}
+                    />
+                  </div>
+                </ActionIcon>
+              </Menu.Target>
+              <Menu.Dropdown>
+                {action.map((action, index) => (
+                  <Menu.Item
+                    // eslint-disable-next-line react/no-array-index-key
+                    key={index}
+                    color={action.color}
+                    icon={action.icon}
+                  >
+                    {action.label}
+                  </Menu.Item>
+                ))}
+              </Menu.Dropdown>
+            </Menu>
+          )}
         </div>
       </Group>
-      {image}
+      <Image radius="16px" src={image} />
     </Box>
   );
 }
