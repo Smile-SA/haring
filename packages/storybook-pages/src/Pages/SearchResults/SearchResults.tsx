f6a031dd50c30be416f01941f4841faa17561e51
vapersmile <valentin.perello@smile.fr>
2023-12-06 17:22:07 +0100

    feat(redmine 1245005): add dynamique data for search results

--- packages/storybook-pages/src/Pages/SearchResults/SearchResults.tsx
+++ packages/storybook-pages/src/Pages/SearchResults/SearchResults.tsx
@@ -26,7 +26,7 @@ import {
 } from '@phosphor-icons/react';
 import {
   DocumentCard,
-  Filters,
+  FiltersBar,
   FoldableColumnLayout,
   Header,
   Motif,
@@ -78,12 +78,195 @@ const useStyles = createStyles((theme) => ({
 export function SearchResults(): ReactElement {
   // style
   const theme = useMantineTheme();
+  const { classes } = useStyles();
   const numberOfResults = 135;
   // Search
   const [search, setSearch] = useState<string>('567890456');
   // Pagination
   const [page, setPage] = useState<number>(1);
   const [rowsPerPage, setRowsPerPage] = useState<number>(10);
+  // FilterBar
+  const [activeFilters, setActiveFilters] = useState([
+    {
+      categoryId: [1],
+      id: 1,
+      label: 'dupont',
+      onRemove: () => {
+        setActiveFilters(activeFilters.filter((filter) => filter.id !== 1));
+      },
+      value: 'dupont',
+    },
+    {
+      categoryId: [2, 4],
+      id: 2,
+      label: 'particulier',
+      onRemove: () => {
+        setActiveFilters(activeFilters.filter((filter) => filter.id !== 2));
+      },
+      value: 'particular',
+    },
+  ]);
+  const [dupontFilter, setDupontFilter] = useState(true);
+  const [martinFilter, setMartinFilter] = useState(false);
+  const [andreeFilter, setAndreeFilter] = useState(false);
+  const [cdiFilter, setCdiFilter] = useState(false);
+  const [cddFilter, setCddFilter] = useState(false);
+  const [freelanceFilter, setFreelanceFilter] = useState(false);
+  const [particularFilter, setParticularFilter] = useState(true);
+  const menus = [
+    {
+      content: (
+        <div>
+          <Group>
+            <Checkbox
+              checked={dupontFilter}
+              onChange={(event) => {
+                setActiveFiltersManager(1, event.target);
+              }}
+              value="dupont"
+            />{' '}
+            Dupont
+          </Group>
+          <br />
+          <Group>
+            <Checkbox
+              checked={martinFilter}
+              onChange={(event) => {
+                setActiveFiltersManager(2, event.target);
+              }}
+              value="martin"
+            />{' '}
+            Martin
+          </Group>
+          <br />
+          <Group>
+            <Checkbox
+              checked={andreeFilter}
+              onChange={(event) => {
+                setActiveFiltersManager(3, event.target);
+              }}
+              value="andré"
+            />{' '}
+            Andrée
+          </Group>
+        </div>
+      ),
+      id: 1,
+      label: `Nom du client`,
+    },
+    {
+      children: [
+        {
+          content: (
+            <>
+              <Group>
+                <Checkbox
+                  checked={cdiFilter}
+                  onChange={(event) => {
+                    setActiveFiltersManager(4, event.target);
+                  }}
+                  value="CDI"
+                />{' '}
+                CDI
+              </Group>
+              <br />
+              <Group>
+                <Checkbox
+                  checked={cddFilter}
+                  onChange={(event) => {
+                    setActiveFiltersManager(5, event.target);
+                  }}
+                  value="CDD"
+                />{' '}
+                CDD
+              </Group>
+            </>
+          ),
+          id: 3,
+          label: 'Contrat classique',
+        },
+        {
+          content: (
+            <>
+              <Group>
+                <Checkbox
+                  checked={freelanceFilter}
+                  onChange={(event) => {
+                    setActiveFiltersManager(6, event.target);
+                  }}
+                  value="freelance"
+                />{' '}
+                Freelance
+              </Group>
+              <br />
+              <Group>
+                <Checkbox
+                  checked={particularFilter}
+                  onChange={(event) => {
+                    setActiveFiltersManager(7, event.target);
+                  }}
+                  value="particular"
+                />{' '}
+                Particulié
+              </Group>
+            </>
+          ),
+          id: 4,
+          label: 'Contrat special',
+        },
+      ],
+      id: 2,
+      label: 'Type de contrat',
+    },
+    {
+      content: (
+        <div className={classes.periodContainer}>
+          <DateInput
+            classNames={{
+              input: classes.dataInput,
+              label: classes.dataInputLabel,
+            }}
+            label="Entre le"
+            placeholder="JJ /MM/ AAAA"
+            rightSection={
+              <CalendarBlank
+                color={theme.colors.cyan[9]}
+                size={20}
+                weight="bold"
+              />
+            }
+            value={new Date('01/05/12')}
+            valueFormat="DD/MM/YYYY"
+          />
+          <Space h="6px" />
+          <DateInput
+            classNames={{
+              input: classes.dataInput,
+              label: classes.dataInputLabel,
+            }}
+            label="Et le"
+            placeholder="JJ /MM/ AAAA"
+            rightSection={
+              <CalendarBlank
+                color={theme.colors.cyan[9]}
+                size={20}
+                weight="bold"
+              />
+            }
+            value={new Date('05/23/12')}
+            valueFormat="DD/MM/YYYY"
+          />
+        </div>
+      ),
+      id: 5,
+      label: 'Période',
+    },
+    { id: 6, label: 'Durée du contrat' },
+    { id: 7, label: 'Nom du filtre' },
+    { id: 8, label: 'Nom du filtre' },
+    { id: 9, label: 'Nom du filtre' },
+  ];
+
   const rowsPerPageOptions = [
     { label: 'Afficher 5 résultats', value: 5 },
     { label: 'Afficher 10 résultats', value: 10 },
@@ -130,111 +313,80 @@ export function SearchResults(): ReactElement {
   );
 
   const totalPages = Math.ceil(typeFilteredResults / rowsPerPage);
-  const { classes } = useStyles();
-  const menu = [
-    {
-      content: (
-        <div>
-          <Group>
-            <Checkbox checked value="Dupont" /> Dupont
-          </Group>
-          <br />
-          <Group>
-            <Checkbox value="Martin" /> Martin
-          </Group>
-          <br />
-          <Group>
-            <Checkbox value="André" /> Andrée
-          </Group>
-        </div>
-      ),
-      id: 1,
-      label: `Nom du client`,
-    },
-    {
-      children: [
-        {
-          content: (
-            <>
-              <Group>
-                <Checkbox value="CDI" /> CDI
-              </Group>
-              <br />
-              <Group>
-                <Checkbox value="CDD" /> CDD
-              </Group>
-            </>
-          ),
-          id: 10,
-          label: 'Contrat classique',
-        },
-        {
-          content: (
-            <>
-              <Group>
-                <Checkbox value="FREELANCE" /> Freelance
-              </Group>
-              <br />
-              <Group>
-                <Checkbox checked value="PARTICULIER" /> Particulié
-              </Group>
-            </>
-          ),
-          id: 11,
-          label: 'Contrat special',
-        },
-      ],
-      id: 3,
-      label: 'Type de contrat',
-    },
-    {
-      content: (
-        <div className={classes.periodContainer}>
-          <DateInput
-            classNames={{
-              input: classes.dataInput,
-              label: classes.dataInputLabel,
-            }}
-            label="Entre le"
-            placeholder="JJ /MM/ AAAA"
-            rightSection={
-              <CalendarBlank
-                color={theme.colors.cyan[9]}
-                size={20}
-                weight="bold"
-              />
-            }
-            value={new Date('01/05/12')}
-            valueFormat="DD/MM/YYYY"
-          />
-          <Space h="6px" />
-          <DateInput
-            classNames={{
-              input: classes.dataInput,
-              label: classes.dataInputLabel,
-            }}
-            label="Et le"
-            placeholder="JJ /MM/ AAAA"
-            rightSection={
-              <CalendarBlank
-                color={theme.colors.cyan[9]}
-                size={20}
-                weight="bold"
-              />
-            }
-            value={new Date('05/23/12')}
-            valueFormat="DD/MM/YYYY"
-          />
-        </div>
-      ),
-      id: 8,
-      label: 'Période',
-    },
-    { id: 9, label: 'Durée du contrat' },
-    { id: 10, label: 'Nom du filtre' },
-    { id: 23, label: 'Nom du filtre' },
-    { id: 143, label: 'Nom du filtre' },
-  ];
+
+  function removeAllFilters(): void {
+    setActiveFilters([]);
+    setDupontFilter(false);
+    setMartinFilter(false);
+    setAndreeFilter(false);
+    setCdiFilter(false);
+    setCddFilter(false);
+    setFreelanceFilter(false);
+    setParticularFilter(false);
+  }
+
+  const setActiveFiltersManager = (
+    id: number,
+    element: EventTarget & HTMLInputElement,
+  ): void => {
+    let categoryId: number[] = [];
+    let currentSetFilter: (() => void) | undefined = undefined;
+    switch (id) {
+      case 1:
+        currentSetFilter = () => setDupontFilter(element.checked);
+        categoryId = [1];
+        break;
+      case 2:
+        currentSetFilter = () => setMartinFilter(element.checked);
+        categoryId = [1];
+        break;
+      case 3:
+        currentSetFilter = () => setAndreeFilter(element.checked);
+        categoryId = [1];
+        break;
+      case 4:
+        currentSetFilter = () => setCdiFilter(element.checked);
+        categoryId = [2, 3];
+        break;
+      case 5:
+        currentSetFilter = () => setCddFilter(element.checked);
+        categoryId = [2, 3];
+        break;
+      case 6:
+        currentSetFilter = () => setFreelanceFilter(element.checked);
+        categoryId = [2, 4];
+        break;
+      case 7:
+        currentSetFilter = () => setParticularFilter(element.checked);
+        categoryId = [2, 4];
+        break;
+    }
+    currentSetFilter?.();
+
+    if (id >= 0 && id <= 7) {
+      const filtersId = activeFilters.map((filter) => filter.id);
+
+      if (filtersId.includes(id)) {
+        setActiveFilters(activeFilters.filter((element) => element.id !== id));
+      } else {
+        setActiveFilters([
+          ...activeFilters,
+          {
+            categoryId,
+            id,
+            label: element.value,
+            onRemove: () => {
+              currentSetFilter?.();
+              setActiveFilters(
+                activeFilters.filter((filter) => filter.id !== id),
+              );
+            },
+            value: element.value,
+          },
+        ]);
+      }
+    }
+  };
 
   return (
     <AppShell
@@ -256,31 +408,13 @@ export function SearchResults(): ReactElement {
         isColumnVisible={isColumnVisible}
         onChangeIsColumnVisible={setIsColumnVisible}
         sidebarContent={
-          <Filters
-            activeFilters={[
-              {
-                categoryId: [1],
-                id: 1,
-                label: 'dupont',
-                value: 'DUPONT',
-              },
-              {
-                categoryId: [3, 11],
-                id: 2,
-                label: 'particulier',
-                value: 'PARTICULAR',
-              },
-              {
-                categoryId: [8],
-                id: 3,
-                label: '3 ans',
-                value: '3 YEARS',
-              },
-            ]}
+          <FiltersBar
+            activeFilters={activeFilters}
+            defaultOpenedMenuIds={[8]}
             deleteButtonLabel="Supprimer tout"
             filterButtonLabel="Filtrer"
-            openedMenuIds={[8]}
-            sideBarFiltersMenu={menu}
+            menus={menus}
+            onDeleteButtonClick={removeAllFilters}
             title="Filtres actifs"
           />
         }
