84269a9f0f84652e7388efd8cba220a00a9e1f32
vapersmile <valentin.perello@smile.fr>
2023-12-14 11:20:25 +0100

    feat(redmine 1267924): fix error test

--- packages/react-front-kit/src/Components/FiltersCheckboxList/FiltersCheckboxList.test.tsx
+++ packages/react-front-kit/src/Components/FiltersCheckboxList/FiltersCheckboxList.test.tsx
@@ -1,14 +1,13 @@
 import { renderWithProviders } from '@smile/react-front-kit-shared/test-utils';
 
 import { FiltersCheckboxList } from './FiltersCheckboxList';
-import { filters } from './FiltersCheckboxList.mock';
 
 describe('FiltersCheckboxList', () => {
   it('matches snapshot', () => {
     const { container } = renderWithProviders(
       <FiltersCheckboxList
         buttonLabel="Test"
-        filters={filters}
+        filters={[]}
         placeholder="Test"
       />,
     );
