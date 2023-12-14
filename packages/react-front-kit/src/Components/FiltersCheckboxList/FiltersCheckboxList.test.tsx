e43960b03e2b9d28cc86a17373be98397fd45f65
vapersmile <valentin.perello@smile.fr>
2023-12-14 15:15:59 +0100

    feat(redmine 1267924): add math.random for prevent mantine random ID

--- packages/react-front-kit/src/Components/FiltersCheckboxList/FiltersCheckboxList.test.tsx
+++ packages/react-front-kit/src/Components/FiltersCheckboxList/FiltersCheckboxList.test.tsx
@@ -1,13 +1,18 @@
 import { renderWithProviders } from '@smile/react-front-kit-shared/test-utils';
 
 import { FiltersCheckboxList } from './FiltersCheckboxList';
+import { filters } from './FiltersCheckboxList.mock';
 
 describe('FiltersCheckboxList', () => {
+  beforeEach(() => {
+    // Prevent mantine random ID
+    Math.random = () => 0.42;
+  });
   it('matches snapshot', () => {
     const { container } = renderWithProviders(
       <FiltersCheckboxList
         buttonLabel="Test"
-        filters={[]}
+        filters={filters}
         placeholder="Test"
       />,
     );
