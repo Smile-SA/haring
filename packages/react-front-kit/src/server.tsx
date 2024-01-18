45fcd9edc93ed60ff9548b579d7f381cae4d8bf9
Tony CABAYE <tony.cabaye@smile.fr>
2024-01-18 15:31:08 +0100

    fix: server export

--- packages/react-front-kit/src/server.tsx
+++ packages/react-front-kit/src/server.tsx
@@ -1,4 +1,8 @@
 /* eslint-disable react-refresh/only-export-components */
-export * from '@smile/react-front-kit-shared/server';
-// other exports
-export * from './helpers';
+// helper exports
+export {
+  addPathAndDepth,
+  flattenNestedObjects,
+  getClosestItemFromPath,
+  setChildrenToTree,
+} from './helpers';
