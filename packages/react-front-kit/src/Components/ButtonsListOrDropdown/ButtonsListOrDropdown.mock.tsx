b7c78a6ccecb92d9b42599a71948918bf950758e
vapersmile <valentin.perello@smile.fr>
2024-01-18 09:36:36 +0100

    feat(redmine 1273832): add mock for items and fix id errors for test

---
+++ packages/react-front-kit/src/Components/ButtonsListOrDropdown/ButtonsListOrDropdown.mock.tsx
@@ -0,0 +1,5 @@
+export const items = [
+  { label: 'FR', value: 'FR' },
+  { label: 'EN', value: 'EN' },
+  { label: 'ES', value: 'ES' },
+];
--- packages/react-front-kit/src/Components/ButtonsListOrDropdown/ButtonsListOrDropdown.stories.tsx
+++ packages/react-front-kit/src/Components/ButtonsListOrDropdown/ButtonsListOrDropdown.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { ButtonsListOrDropdown as Cmp } from './ButtonsListOrDropdown';
+import { items } from './ButtonsListOrDropdown.mock';
 
 const meta = {
   component: Cmp,
@@ -15,10 +16,6 @@ export const ButtonsListOrDropdown: IStory = {
   args: {
     defaultCurrent: 'FR',
     displayAll: true,
-    items: [
-      { label: 'FR', value: 'FR' },
-      { label: 'EN', value: 'EN' },
-      { label: 'ES', value: 'ES' },
-    ],
+    items,
   },
 };
