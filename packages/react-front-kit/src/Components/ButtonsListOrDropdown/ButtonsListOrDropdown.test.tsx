3be049645461b7b53b5b8d413a50300f996f8802
vapersmile <valentin.perello@smile.fr>
2024-01-18 09:36:36 +0100

    feat(redmine 1273832): add mock for items and fix id errors for test

--- packages/react-front-kit/src/Components/ButtonsListOrDropdown/ButtonsListOrDropdown.test.tsx
+++ packages/react-front-kit/src/Components/ButtonsListOrDropdown/ButtonsListOrDropdown.test.tsx
@@ -1,17 +1,16 @@
 import { renderWithProviders } from '@smile/react-front-kit-shared/test-utils';
 
 import { ButtonsListOrDropdown } from './ButtonsListOrDropdown';
+import { items } from './ButtonsListOrDropdown.mock';
 
 describe('ButtonsListOrDropdown', () => {
+  beforeEach(() => {
+    // Prevent mantine random ID
+    Math.random = () => 0.42;
+  });
   it('matches snapshot', () => {
     const { container } = renderWithProviders(
-      <ButtonsListOrDropdown
-        defaultCurrent=""
-        items={[]}
-        onAction={function (): void {
-          throw new Error('Function not implemented.');
-        }}
-      />,
+      <ButtonsListOrDropdown defaultCurrent="FR" items={items} />,
     );
     expect(container).toMatchSnapshot();
   });
