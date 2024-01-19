7cbd1e7befa1a0703ba3ac4cc0213c331716e824
Quentin Le Caignec <12102823+QuentinLeCaignec@users.noreply.github.com>
2024-01-19 17:04:12 +0100

    refactor(redmine 1274147): used SidebarMenu in SidebarFilters instead… (#115)
    * refactor(redmine 1274147): used SidebarMenu in SidebarFilters instead of clone, improved SidebarMenu

--- packages/react-front-kit/src/Components/SidebarFilters/SidebarFilters.mock.tsx
+++ packages/react-front-kit/src/Components/SidebarFilters/SidebarFilters.mock.tsx
@@ -1,3 +1,7 @@
+import type { IMenuItem } from '../SidebarMenu/SidebarMenu';
+
+import { Checkbox, Group } from '@mantine/core';
+
 export const activeFilters = [
   {
     categoryId: [1],
@@ -24,3 +28,62 @@ export const activeFilters = [
     value: 'CDI',
   },
 ];
+
+export function getMenu(checked: boolean): IMenuItem<number>[] {
+  return [
+    {
+      content: (
+        <>
+          <Group>
+            <Checkbox defaultChecked={checked} value="Dupont" /> Dupont
+          </Group>
+          <br />
+          <Group>
+            <Checkbox defaultChecked={checked} value="Martin" /> Martin
+          </Group>
+          <br />
+          <Group>
+            <Checkbox value="André" /> Andrée
+          </Group>
+        </>
+      ),
+      id: 1,
+      label: 'Nom du client',
+    },
+    {
+      children: [
+        {
+          content: (
+            <>
+              <Group>
+                <Checkbox defaultChecked={checked} value="CDI" /> CDI
+              </Group>
+              <br />
+              <Group>
+                <Checkbox value="CDD" /> CDD
+              </Group>
+            </>
+          ),
+          id: 10,
+          label: 'Contrat classique',
+        },
+        {
+          content: (
+            <Group>
+              <Checkbox defaultChecked={checked} value="FREELANCE" /> Freelance
+            </Group>
+          ),
+          id: 11,
+          label: 'Contrat special',
+        },
+      ],
+      id: 3,
+      label: 'Type de contrat',
+    },
+    {
+      id: 8,
+      label: 'Période',
+    },
+    { id: 9, label: 'Durée du contrat' },
+  ];
+}
