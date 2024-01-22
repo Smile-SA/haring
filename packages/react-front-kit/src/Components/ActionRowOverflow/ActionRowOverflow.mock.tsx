159e6531cb7039d842918f035aa37c9825f26d23
Quentin Le Caignec <12102823+QuentinLeCaignec@users.noreply.github.com>
2024-01-22 10:37:50 +0100

    feat(redmine 1274413): extracted logic from ActionBar into new ActionRowOverflow (#119)

---
+++ packages/react-front-kit/src/Components/ActionRowOverflow/ActionRowOverflow.mock.tsx
@@ -0,0 +1,70 @@
+import type { IActionRowOverflowProps } from './ActionRowOverflow';
+import type { IThumbnail, IThumbnailAction } from '../../types';
+
+import { Trash } from '@phosphor-icons/react';
+import { FolderMove } from '@smile/react-front-kit-shared';
+import { action } from '@storybook/addon-actions';
+
+export const actionRowOverflowActionsMock: IThumbnailAction[] = [
+  {
+    icon: <FolderMove size={16} />,
+    id: 'move',
+    isItemAction: false,
+    isMassAction: true,
+    label: 'Move in tree',
+    onAction: action('Move selected in tree'),
+  },
+  {
+    color: 'red',
+    confirmModalProps: {
+      cancelLabel: 'Abort',
+      children: <p>Are you sure ?</p>,
+      confirmColor: 'red',
+      confirmLabel: 'Remove',
+      title: 'Remove files ?',
+    },
+    confirmation: true,
+    icon: <Trash size={16} />,
+    id: 'delete',
+    isItemAction: false,
+    isMassAction: true,
+    label: 'Delete',
+    onAction: action('Delete selected'),
+  },
+  {
+    icon: <FolderMove size={16} />,
+    id: 'test1',
+    isItemAction: true,
+    isMassAction: false,
+    label: 'Overflow Test 1',
+    onAction: action('Overflow Test 1'),
+  },
+  {
+    icon: <FolderMove size={16} />,
+    id: 'test2',
+    isItemAction: false,
+    isMassAction: true,
+    label: 'Overflow Test 2',
+    onAction: action('Overflow Test 2'),
+  },
+];
+
+export const actionRowOverflowSelectedMock: IThumbnail[] = [
+  {
+    id: '1',
+    label: 'Label A',
+  },
+  {
+    id: '2',
+    label: 'Label B',
+  },
+  {
+    id: '3',
+    label: 'Label C',
+  },
+];
+
+export const actionRowOverflowMock: IActionRowOverflowProps<IThumbnail> = {
+  actions: actionRowOverflowActionsMock,
+  selectedElements: actionRowOverflowSelectedMock,
+};
--- packages/react-front-kit/src/Components/ActionRowOverflow/ActionRowOverflow.stories.tsx
+++ packages/react-front-kit/src/Components/ActionRowOverflow/ActionRowOverflow.stories.tsx
@@ -1,30 +1,27 @@
-import type { IActionRowOverflowAction } from '../ActionRowOverflow/ActionRowOverflow';
+import type { IActionRowOverflowAction } from './ActionRowOverflow';
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { ActionRowOverflow as Cmp } from './ActionRowOverflow';
 import {
   actionRowOverflowActionsMock,
   actionRowOverflowSelectedMock,
-} from '../ActionRowOverflow/ActionRowOverflow.mock';
-
-import { ActionBar as Cmp } from './ActionBar';
-import { actionBarLabelMock } from './ActionBar.mock';
+} from './ActionRowOverflow.mock';
 
 const meta = {
   component: Cmp,
   tags: ['autodocs'],
-  title: '3-custom/Components/ActionBar',
+  title: '3-custom/Components/ActionRowOverflow',
 } satisfies Meta<typeof Cmp>;
 
 export default meta;
 type IStory = StoryObj<typeof meta>;
 
-export const ActionBar: IStory = {
+export const ActionRowOverflow: IStory = {
   args: {
     actions: actionRowOverflowActionsMock as IActionRowOverflowAction<
       Record<string, unknown>
     >[],
     rowActionNumber: 2,
     selectedElements: actionRowOverflowSelectedMock,
-    selectedElementsLabel: actionBarLabelMock,
   },
 };
