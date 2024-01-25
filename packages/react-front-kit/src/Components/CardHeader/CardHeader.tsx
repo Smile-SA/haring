2d4ea740aac3ea0d2edebd572b465249489cf234
vapersmile <120370047+vapersmile@users.noreply.github.com>
2024-01-25 15:25:25 +0100

    feat(redmine 1274751): add CardHeader (#122)
    * feat(redmine 1274751): change component name
    
    * feat(redmine 1274751): fix style from mock
    
    * feat(redmine 1274751): add Card on story and add BoxProps
    
    * feat(redmine 1274751): change style
    
    * feat(redmine 1274751): add boxShadow on root
    
    * feat(redmine 1274751): add boxShadow on root

---
+++ packages/react-front-kit/src/Components/CardHeader/CardHeader.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import type { BoxProps } from '@mantine/core';
+import type { ReactElement, ReactNode } from 'react';
+
+import { Box, createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    display: 'flex',
+    gap: '16px',
+  },
+  root: {
+    backgroundColor: theme.fn.primaryColor(),
+    boxShadow: '0px 8.687101364135742px 6.949680805206299px 0px #00000006',
+    color: theme.white,
+    padding: '24px 32px',
+  },
+}));
+
+export interface ICardHeaderProps extends BoxProps {
+  children: ReactNode;
+  leftSection?: ReactNode;
+  rightSection?: ReactNode;
+}
+
+export function CardHeader(props: ICardHeaderProps): ReactElement {
+  const { children, leftSection, rightSection, ...rootProps } = props;
+  const { classes } = useStyles();
+  return (
+    <Box className={classes.root} {...rootProps}>
+      <div className={classes.container}>
+        {Boolean(leftSection) && <div>{leftSection}</div>}
+        <div>{children}</div>
+        {Boolean(rightSection) && <div>{rightSection}</div>}
+      </div>
+    </Box>
+  );
+}
