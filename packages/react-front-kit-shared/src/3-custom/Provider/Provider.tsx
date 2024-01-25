2e4dd3c2d473cee7e063ce5beb38eaaf5593802f
Quentin Le Caignec <quentin.lecaignec@smile.fr>
2024-01-25 17:32:57 +0100

    refactor: wip, marked problems with TODO

--- packages/react-front-kit-shared/src/3-custom/Provider/Provider.tsx
+++ packages/react-front-kit-shared/src/3-custom/Provider/Provider.tsx
@@ -12,17 +12,19 @@ export interface IProviderProps extends IThemeOverride {
   children?: ReactNode;
   colorScheme?: 'dark' | 'light';
 }
+
 export function Provider(props: IProviderProps): ReactElement {
-  const { children, colorScheme = 'light', ...themeConfig } = props;
+  const { children /* , colorScheme = 'light'*/, ...themeConfig } = props;
   const themes = createThemes(themeConfig);
   const { main } = themes;
 
   return (
     <themeContext.Provider value={themes}>
       <MantineProvider
-        theme={{ ...main, colorScheme }}
-        withGlobalStyles
-        withNormalizeCSS
+        // theme={{ ...main, colorScheme }} // TODO, colorScheme no longer exists
+        theme={{ ...main }}
+        // withGlobalStyles // TODO: props no longer exists
+        // withNormalizeCSS / TODO: props no longer exists
       >
         {children}
       </MantineProvider>
