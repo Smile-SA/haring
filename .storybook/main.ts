727c9bc101143933904f7ad6c47140418b4e783a
vapersmile <valentin.perello@smile.fr>
2024-01-31 17:13:37 +0100

    feat(redmine 1275779): add summaryBox component for migration V7

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -36,6 +36,8 @@ const config: StorybookConfig = {
     '../packages/react-front-kit/src/Components/SearchableCheckboxList/*.stories.tsx',
     '../packages/react-front-kit/src/Components/SelectableList/*.stories.tsx',
     '../packages/react-front-kit/src/Components/SidebarMenu/*.stories.tsx',
+    '../packages/react-front-kit/src/Components/SwitchableView/*.stories.tsx',
+    '../packages/react-front-kit/src/Components/SummaryBox/*.stories.tsx',
     // '../packages/*/src/**/*.mdx', // TODO: once all components are migrated, revert to this
     // '../packages/*/src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
