25accc10c5fe1b711992b9a816359a076f13bd87
Quentin Le Caignec <12102823+QuentinLeCaignec@users.noreply.github.com>
2024-02-27 09:33:58 +0100

    Feat/calendar header (#153)
    * feat: added react-front-kit-dates package
    
    * feat: added CalendarHeader component on top of mantine's component

---
+++ packages/react-front-kit-dates/src/Components/CalendarHeader/CalendarHeader.test.tsx
@@ -0,0 +1,20 @@
+import { renderWithProviders } from '@smile/react-front-kit-shared/test-utils';
+
+import { CalendarHeader } from './CalendarHeader';
+
+describe('CalendarHeader', () => {
+  beforeEach(() => {
+    // Prevent mantine random ID
+    Math.random = () => 0.42;
+  });
+  it('matches snapshot', () => {
+    const { container } = renderWithProviders(
+      <CalendarHeader
+        date={new Date(0, 0, 0)}
+        label="August 2023"
+        level="month"
+      />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
