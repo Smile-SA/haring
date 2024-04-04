fe93b0453ecc0efbfd6fb30a9933ca8ce82ad3ad
vapersmile <120370047+vapersmile@users.noreply.github.com>
2024-04-04 14:28:50 +0200

    Feat/fetch autocomplete field (#167)
    * feat(redmine 1286242): add addressField Component
    
    * feat: add useDebouncedState
    
    * feat(redmine 1286242): fix api error and add all features
    
    * chore: rebase 0.11.x
    
    * feat(redmine 1286242): add addressField Component
    
    * feat(redmine 1286242): fix api error and add all features
    
    * feat(redmine 1286242): add FetchAutoCompleteField
    
    * feat(redmine 1286242): update the component
    
    * feat(redmine 1286242): add transformResultsFunction
    
    * feat(redmine 1286242): add FetchAutocompleteField
    
    * feat(redmine 1286242): change location of getData function
    
    * feat(redmine 1286242): fix test
    
    ---------
    
    Co-authored-by: pereag <perello.valentin@hotmail.fr>

---
+++ packages/react-front-kit/src/Form/FetchAutocompleteField/FetchAutoCompleteField.mock.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export interface IOpenStreetMapData {
+  display_name: string;
+}
+
+export interface IAdressGouvData {
+  properties: { label: string };
+}
+
+export async function getDataOpenStreetMapMock(
+  value: string,
+): Promise<unknown> {
+  const response = await fetch(
+    `https://nominatim.openstreetmap.org/search.php?q=${encodeURIComponent(
+      value,
+    )}&format=jsonv2&addressdetails=1&countrycodes=fr&accept-language=fr&limit=10&dedupe=1`,
+  );
+  const data: IOpenStreetMapData[] = await response.json();
+  const result = data.map((element) => {
+    return { label: element.display_name, value: element };
+  });
+
+  return result;
+}
+
+export async function getDataAddressGouvMock(value: string): Promise<unknown> {
+  const response = await fetch(
+    `https://api-adresse.data.gouv.fr/search/?q=${encodeURIComponent(
+      value,
+    )}&autocomplete=1`,
+  );
+  const data: { features: IAdressGouvData[] } = await response.json();
+  const result = data.features.map((element) => {
+    return { label: element.properties.label, value: element };
+  });
+
+  return result;
+}
