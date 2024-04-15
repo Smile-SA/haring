eda93250374b606e537ed1ba0e2db315a06c68b7
pereag <perello.valentin@hotmail.fr>
2024-04-15 11:41:06 +0200

    feat (redmine 1291405): add AdressAutoCommpleteForm folder and add changeset

---
+++ packages/react-front-kit/src/Form/AddressAutocompleteForm/AddressAutocompleteForm.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import type { IAddressGouvData } from '../FetchAutocompleteField/FetchAutoCompleteField.mock';
+import type {
+  IFetchAutocompleteFieldProps,
+  IValue,
+} from '../FetchAutocompleteField/FetchAutocompleteField';
+import type { ReactElement } from 'react';
+
+import { FetchAutocompleteField } from '../FetchAutocompleteField/FetchAutocompleteField';
+
+export interface IAddressAutocompleteFieldProps<F>
+  extends Omit<IFetchAutocompleteFieldProps<F>, 'onFetchData'> {
+  lat?: string;
+  limit?: number;
+  lon?: string;
+  onFetchData?: (value: string) => Promise<IValue<F>[]>;
+  type?: string;
+}
+
+export function AddressGouvAutocompleteField<F>(
+  props: IAddressAutocompleteFieldProps<F>,
+): ReactElement {
+  const {
+    lat = '',
+    limit = 10,
+    lon = '',
+    type = '',
+    ...fetchAutocompleteFieldProps
+  } = props;
+  async function getDataAddressGouv(value: string): Promise<IValue<unknown>[]> {
+    const response = await fetch(
+      `https://api-Adresse.data.gouv.fr/search/?q=${encodeURIComponent(
+        value,
+      )}&autocomplete=1&lat=${lat}&lon=${lon}&type=${type}&limit=${limit}`,
+    );
+    const data: { features: IAddressGouvData[] } = await response.json();
+    const result = data.features.map((element) => {
+      return { label: element.properties.label, value: element };
+    });
+
+    return result;
+  }
+  return (
+    <FetchAutocompleteField
+      {...fetchAutocompleteFieldProps}
+      onFetchData={getDataAddressGouv}
+    />
+  );
+}
