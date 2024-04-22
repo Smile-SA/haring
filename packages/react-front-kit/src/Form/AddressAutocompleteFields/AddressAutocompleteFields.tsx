fb73fb007df782363ee8bfcbdd5406d21ab0c7ea
pereag <perello.valentin@hotmail.fr>
2024-04-22 15:54:06 +0200

    feat: started modifs

--- packages/react-front-kit/src/Form/AddressAutocompleteFields/AddressAutocompleteFields.tsx
+++ packages/react-front-kit/src/Form/AddressAutocompleteFields/AddressAutocompleteFields.tsx
@@ -1,4 +1,5 @@
 'use client';
+import type { IAddressFields } from '../../../dist/Form/AddressAutocompleteFields/AddressAutocompleteFields';
 import type {
   IFetchAutocompleteFieldProps,
   IValue,
@@ -6,57 +7,16 @@ import type {
 import type { TextInputProps } from '@mantine/core';
 import type { ReactElement } from 'react';
 
-import { TextInput } from '@mantine/core';
 import { useState } from 'react';
 
+import { AddressFields } from '../AddressFields/AddressFields';
 import { FetchAutocompleteField } from '../FetchAutocompleteField/FetchAutocompleteField';
-
-import classes from './AddressAutocompleteFields.module.css';
-
-export interface IAddressFields {
-  city?: string;
-  country?: string;
-  number?: string;
-  postCode?: string;
-  street?: string;
-}
-
-export interface ICity {
-  description?: string;
-  label?: string;
-  placeholder?: string;
-}
-export interface ICountry {
-  description?: string;
-  label?: string;
-  placeholder?: string;
-}
-export interface INumber {
-  description?: string;
-  label?: string;
-  placeholder?: string;
-}
-export interface IPostcode {
-  description?: string;
-  label?: string;
-  placeholder?: string;
-}
-
-export interface IStreet {
-  description?: string;
-  label?: string;
-  placeholder?: string;
-}
 
 export interface IAddressAutocompleteFieldsProps<T>
   extends Omit<IFetchAutocompleteFieldProps<T>, 'onOptionSubmit'> {
-  city?: ICity;
-  country?: ICountry;
-  number?: INumber;
+  addressFieldsProps: IAddressFields;
   onFieldsValuesChange?: (value: IAddressFields) => void;
   onOptionSubmit: (value: IValue<T>) => IAddressFields;
-  postCode?: IPostcode;
-  street?: IStreet;
   textInputProps?: TextInputProps;
 }
 
@@ -64,22 +24,9 @@ export function AddressAutocompleteFields<T>(
   props: IAddressAutocompleteFieldsProps<T>,
 ): ReactElement {
   const {
-    street = {
-      label: 'Street Name',
-      placeholder: 'Pall Mall',
-    },
-    city = {
-      label: 'City',
-      placeholder: 'London',
-    },
-    country = { label: 'Country', placeholder: 'United Kingdom' },
-    number = { label: 'Street number', placeholder: '89' },
+    addressFieldsProps,
     onOptionSubmit,
     onFieldsValuesChange,
-    postCode = {
-      label: 'Postal code',
-      placeholder: 'SW1Y 5HS',
-    },
     textInputProps,
     ...fetchAutocompleteFieldProps
   } = props;
@@ -99,86 +46,21 @@ export function AddressAutocompleteFields<T>(
     setCountryValue(addressFields.country ?? '');
   }
 
-  function onChangeHandle(label: string, value: string): void {
-    onFieldsValuesChange?.({ [label]: value });
-  }
-
-  const inputs = [
-    {
-      description: street.description,
-      label: street.label,
-      onchange: (e: React.ChangeEvent<HTMLInputElement>) => {
-        setStreetValue(e.target.value);
-        onChangeHandle('street', e.target.value);
-      },
-      placeholder: street.placeholder,
-      value: streetValue,
-    },
-    {
-      description: number.description,
-      label: number.label,
-      onchange: (e: React.ChangeEvent<HTMLInputElement>) => {
-        setNumberValue(e.target.value);
-        onChangeHandle('number', e.target.value);
-      },
-      placeholder: number.placeholder,
-      value: numberValue,
-    },
-    {
-      description: city.description,
-      label: city.label,
-      onchange: (e: React.ChangeEvent<HTMLInputElement>) => {
-        setCityValue(e.target.value);
-        onChangeHandle('city', e.target.value);
-      },
-      placeholder: city.placeholder,
-      value: cityValue,
-    },
-    {
-      description: postCode.description,
-      label: postCode.label,
-      onchange: (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPostCodeValue(e.target.value);
-        onChangeHandle('postCode', e.target.value);
-      },
-      placeholder: postCode.placeholder,
-      value: postCodeValue,
-    },
-    {
-      description: country.description,
-      label: country.label,
-      onchange: (e: React.ChangeEvent<HTMLInputElement>) => {
-        setCountryValue(e.target.value);
-        onChangeHandle('country', e.target.value);
-      },
-      placeholder: country.placeholder,
-      value: countryValue,
-    },
-  ];
-
   return (
     <div>
       <FetchAutocompleteField
         onOptionSubmit={(value) => onOptionSubmitHandle(value)}
         {...fetchAutocompleteFieldProps}
       />
-      <div className={classes.inputContainer}>
-        {inputs.map((input) => {
-          return (
-            <TextInput
-              key={input.label}
-              className={classes.input}
-              description={input.description}
-              label={input.label}
-              onChange={(e) => {
-                input.onchange(e);
-              }}
-              placeholder={input.placeholder}
-              value={input.value}
-            />
-          );
-        })}
-      </div>
+      <AddressFields
+        value={{
+          city: cityValue,
+          country: countryValue,
+          number: numberValue,
+          postCode: postCodeValue,
+          street: streetValue,
+        }}
+      />
     </div>
   );
 }
