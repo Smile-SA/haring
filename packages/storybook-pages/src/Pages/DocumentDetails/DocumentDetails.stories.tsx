66f38bc94b4473c9f824deea3e0015200074153e
Tony Cabaye <tony.cabaye@smile.fr>
2024-06-03 12:38:07 +0200

    docs: add storybook themes customization for pages (#185)
    * docs: add storybook themes customization for pages
    
    * fix: page components to apply themes correctly
    
    * test: update snapshot

--- packages/storybook-pages/src/Pages/DocumentDetails/DocumentDetails.stories.tsx
+++ packages/storybook-pages/src/Pages/DocumentDetails/DocumentDetails.stories.tsx
@@ -1,15 +1,16 @@
-'use client';
-
 import type { Meta, StoryObj } from '@storybook/react';
+
+import { sharedMeta } from '../pages-story';
 
 import { DocumentDetails as Cmp } from './DocumentDetails';
 
 const meta = {
+  ...sharedMeta,
   component: Cmp,
   parameters: {
     layout: 'fullscreen',
   },
-  title: '3-Custom/Pages/DocumentDetails',
+  title: '3-custom/Pages/DocumentDetails',
 } satisfies Meta<typeof Cmp>;
 
 export default meta;
