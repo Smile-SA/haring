58f547dd04b497081907c58b7f73c267dcdfc9c4
Quentin LE CAIGNEC <quentin.lecaignec@smile.fr>
2024-06-17 09:26:14 +0200

    feat: wip

---
+++ packages/storybook-pages/src/Pages/AgendaItemPage/AgendaItemPage.mock.tsx
@@ -0,0 +1,16 @@
+import type { IMenuItem } from '@smile/haring-react';
+
+export const menusMock: IMenuItem<string>[] = [
+  {
+    id: '1',
+    label: 'Ordre 1',
+  },
+  {
+    id: '2',
+    label: 'Ordre 2',
+  },
+  {
+    id: '3',
+    label: 'Ordre 3',
+  },
+];
--- packages/storybook-pages/src/Pages/AgendaItemPage/AgendaItemPage.tsx
+++ packages/storybook-pages/src/Pages/AgendaItemPage/AgendaItemPage.tsx
@@ -2,9 +2,15 @@
 
 import type { ReactElement } from 'react';
 
-import { AppShell, Flex, Stack } from '@mantine/core';
-import { FoldableColumnLayout, SidebarMenu } from '@smile/haring-react';
+import { AppShell, Tabs } from '@mantine/core';
+import {
+  Breadcrumbs,
+  FoldableColumnLayout,
+  Pagination,
+  SidebarMenu,
+} from '@smile/haring-react';
 
+import { menusMock } from './AgendaItemPage.mock';
 import classes from './AgendaItemPage.module.css';
 
 export function AgendaItemPage(): ReactElement {
@@ -18,19 +24,40 @@ export function AgendaItemPage(): ReactElement {
             // setGridCols(isVisible ? 4 : 5)
             console.log(isVisible)
           }
-          sidebarContent={<SidebarMenu menu={[]} />}
+          sidebarContent={<SidebarMenu menu={menusMock} />}
           sidebarToggleLabel={toggleLabel}
           topBarRight={
-            <Flex>
-              <span>Fil d&apos;ariane</span>
-            </Flex>
+            <Breadcrumbs>
+              <a href="#">First level</a>
+              <a href="#">Second level</a>
+              <a href="#">Third level</a>
+            </Breadcrumbs>
           }
         >
-          <Stack justify="space-between" style={{ height: 300 }}>
-            <span>Navigation par onglets</span>
-            <p>contenu page</p>
-            <p>pagination</p>
-          </Stack>
+          <div className={classes.box}>
+            <Tabs defaultValue="order">
+              <Tabs.List>
+                <Tabs.Tab value="order">Ordre du jour</Tabs.Tab>
+                <Tabs.Tab value="details">Détails</Tabs.Tab>
+                <Tabs.Tab value="conflicts">Conflits d&apos;intérêt</Tabs.Tab>
+                <Tabs.Tab value="sends">Envois</Tabs.Tab>
+                <Tabs.Tab value="pv">PV</Tabs.Tab>
+                <Tabs.Tab value="decisions">Décisions</Tabs.Tab>
+                <Tabs.Tab value="history">Historique</Tabs.Tab>
+              </Tabs.List>
+            </Tabs>
+            <div className={classes.content}>
+              <p>Contenu page</p>
+            </div>
+            <div className={classes.pagination}>
+              <Pagination
+                itemsPerPage={1}
+                page={1}
+                paginationProps={{ withControls: true }}
+                totalPages={20}
+              />
+            </div>
+          </div>
         </FoldableColumnLayout>
       </AppShell.Main>
     </AppShell>
