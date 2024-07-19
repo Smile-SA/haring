090445676387585e3854bac6296b9dc46c02aadf
Meriemu <me.achemlal@gmail.com>
2024-07-19 09:40:14 +0200

    Feat/stepper page last step (#199)
    * feat(redmine 1301354): add step 3
    
    * feat(redmine 1301354): add changeset file

--- packages/storybook-pages/src/Pages/StepperPage/StepperPage.mock.tsx
+++ packages/storybook-pages/src/Pages/StepperPage/StepperPage.mock.tsx
@@ -2,6 +2,9 @@ export const texts = {
   steps: {
     step1: 'Step 1: ',
     step2: 'Step 2: ',
-    step3: 'Completed, click back button to get to previous step',
+    step3: 'Step 3:',
+    step3SubTitle1: 'Identity: ',
+    step3SubTitle2: 'Address: ',
+    step3Title: 'Summary',
   },
 };
