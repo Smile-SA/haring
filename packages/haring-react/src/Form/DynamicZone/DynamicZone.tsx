f4ea469d052e1a860c0a61c2fec3370cc47448f8
Quentin Le Caignec <12102823+QuentinLeCaignec@users.noreply.github.com>
2024-09-09 14:26:41 +0200

    Feat/dynamic zone (#215)
    * feat: added DynamicZone, related types and utilities
    
    * fix: updated props/types of ActionList for more use-cases

---
+++ packages/haring-react/src/Form/DynamicZone/DynamicZone.tsx
@@ -0,0 +1,103 @@
+import type { IDynamicZoneBlockInternalComponentProps } from './DynamicZoneBlock/DynamicZoneBlock';
+import type { IBaseBlock, IBaseBlockButton, IBaseBlockType } from '../../types';
+import type {
+  CardProps,
+  ContainerProps,
+  GroupProps,
+  StackProps,
+  TextProps,
+} from '@mantine/core';
+import type { ReactElement } from 'react';
+
+import { Button, Container, Group, Stack, Text } from '@mantine/core';
+
+import classes from './DynamicZone.module.css';
+import { DynamicZoneBlock } from './DynamicZoneBlock/DynamicZoneBlock';
+
+export interface IDynamicZoneProps<Block extends IBaseBlock>
+  extends ContainerProps {
+  blockCardProps?: CardProps;
+  blockOptions: IBaseBlockButton[];
+  blocks: Block[];
+  blocksStackProps?: StackProps;
+  bottomContainerProps?: ContainerProps;
+  buttonsGroupProps?: GroupProps;
+  buttonsText?: string;
+  buttonsTextProps?: TextProps;
+  internalBlockCardProps?: IDynamicZoneBlockInternalComponentProps;
+  onAppendBlock: (blockType: IBaseBlockType) => void;
+  onRenderBlockContent: (block: Block, index: number) => ReactElement;
+  onToggleBlock: (block: Block, index: number, opened: boolean) => void;
+}
+
+export function DynamicZone<Block extends IBaseBlock>(
+  props: IDynamicZoneProps<Block>,
+): ReactElement {
+  const {
+    blockCardProps,
+    blockOptions,
+    blocks,
+    blocksStackProps,
+    bottomContainerProps,
+    buttonsGroupProps,
+    buttonsText,
+    buttonsTextProps,
+    internalBlockCardProps,
+    onAppendBlock,
+    onRenderBlockContent,
+    onToggleBlock,
+    ...rootContainerProps
+  } = props;
+
+  function onAddBlock(blockType: IBaseBlockType): void {
+    onAppendBlock(blockType);
+  }
+
+  return (
+    <Container fluid p={0} {...rootContainerProps}>
+      <Stack gap="sm" {...blocksStackProps}>
+        {blocks.map((block, index) => (
+          <DynamicZoneBlock
+            {...blockCardProps}
+            key={block.id}
+            actions={block.blockActions}
+            footerChildren={block.blockFooter}
+            headerChildren={block.blockHeader}
+            internalComponentProps={internalBlockCardProps}
+            onToggle={(opened) => onToggleBlock(block, index, opened)}
+            opened={block.opened}
+            reference={{ arrayLength: blocks.length, id: block.id, index }}
+          >
+            {onRenderBlockContent(block, index)}
+          </DynamicZoneBlock>
+        ))}
+      </Stack>
+      <Container
+        className={classes.buttonsContainer}
+        fluid
+        mt="lg"
+        p="sm"
+        {...bottomContainerProps}
+      >
+        <Text className={classes.buttonsLabel} fw="bold" {...buttonsTextProps}>
+          {buttonsText}
+        </Text>
+        <Group {...buttonsGroupProps}>
+          {blockOptions.map(({ blockType, ...button }) => (
+            <Button
+              radius="md"
+              size="md"
+              type="button"
+              variant="default"
+              {...button}
+              key={`button-${blockType}`}
+              onClick={() => onAddBlock(blockType)}
+            >
+              {button.label}
+            </Button>
+          ))}
+        </Group>
+      </Container>
+    </Container>
+  );
+}
